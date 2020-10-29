d62d293d62fc0bd220b1a96ee0f9f7df0f44b73a
Sean <seanvkamp@gmail.com>
2020-10-29 13:46:33 -0700

    commit tree ae98904a71f90f210d7015d05396a7b0c2121bb7

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,9 @@
-def hello_t
-
+def hello_t(array)
+    array.each do |name|
+        if name.start_with?("T")
+            puts "Hi, #{name}"
+        end
+    end
 end
 
-# call your method here!
 
